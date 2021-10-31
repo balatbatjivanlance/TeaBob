3b6daf7151bebe6ac4de34b9fd3ea3ae99af453b
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-10-31 14:14:27 +0800

    client

--- TeaBob-Mobile/src/app/app-routing.module.ts
+++ TeaBob-Mobile/src/app/app-routing.module.ts
@@ -11,6 +11,22 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'snack',
+    loadChildren: () => import('./pages/snack/snack.module').then( m => m.SnackPageModule)
+  },
+  {
+    path: 'drinks',
+    loadChildren: () => import('./pages/drinks/drinks.module').then( m => m.DrinksPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./pages/cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'status',
+    loadChildren: () => import('./pages/status/status.module').then( m => m.StatusPageModule)
+  },
 ];
 
 @NgModule({
