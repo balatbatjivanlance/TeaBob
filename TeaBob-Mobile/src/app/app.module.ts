27c74f21770543c8be5af5dd8fd13d61b9d044ac
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-11-07 22:34:40 +0800

    update

--- TeaBob-Mobile/src/app/app.module.ts
+++ TeaBob-Mobile/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedComponentsModule } from './components/shared-components.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedComponentsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
