ee9cf3921ea9523291384cb10ab6a5a79e810274
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-11-11 22:56:20 +0800

    new

---
+++ TeaBob-Responsive/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { DrinksComponent } from './pages/drinks/drinks.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SnacksComponent } from './pages/snacks/snacks.component';
+import { StatusComponent } from './pages/status/status.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'snacks', component: SnacksComponent},
+  { path: 'drinks', component: DrinksComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'status', component: StatusComponent},
+  { path: 'profile', component: ProfileComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [
+  HomeComponent,
+  SnacksComponent,
+  DrinksComponent,
+  CartComponent,
+  StatusComponent,
+  ProfileComponent
+]
---
+++ TeaBob-Responsive/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    routingComponents
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
