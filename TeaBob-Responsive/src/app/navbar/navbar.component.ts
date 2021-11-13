0979be8c1e02a657efa096148074b2e5a8c612a1
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-11-13 23:06:14 +0800

    push mo yan ghourl
    with description dapat - afsdghsdghjashdvjcbncvbakjsgfdgfYUGFUkgfuksfdjhcvjsdhvakjshdf

--- TeaBob-Responsive/src/app/navbar/navbar.component.ts
+++ TeaBob-Responsive/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
+    this.pullCart();
+  }
+
+  
+  cartinfo: any={};
+  cart:any;
+  cartCounter: any;
+  
+
+  pullCart() {
+    this.cartinfo.user_id = localStorage.getItem("id");
+    this.ds.sendApiRequest("cart",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
+    this.cart = data.payload;
+
+    // this.getTotal();
+
+    // if(this.cart != null){
+
+    for (let i = 0; i <= this.cart.length; i++) {
+      this.cartCounter = i;
+      console.log(this.cartCounter);
+    }
+    }
+    )
   }
 
 }
--- TeaBob-Responsive/src/app/pages/cart/cart.component.ts
+++ TeaBob-Responsive/src/app/pages/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
+    this.pullCart();
+  }
+
+  
+  cartinfo: any={};
+  cart:any;
+  cartCounter: any;
+  
+
+  pullCart() {
+    this.cartinfo.user_id = localStorage.getItem("id");
+    this.ds.sendApiRequest("cart",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
+    this.cart = data.payload;
+
+    // this.getTotal();
+
+    // if(this.cart != null){
+
+    for (let i = 0; i <= this.cart.length; i++) {
+      this.cartCounter = i;
+      console.log(this.cartCounter);
+    }
+    }
+    )
+  }
+
+  prodinfo: any = {};
+
+  async delCart(e: any) {
+    this.prodinfo.cart_id = e;
+
+        this.ds.sendApiRequest("delCarts", JSON.parse(JSON.stringify(this.prodinfo))).subscribe((data: any) => {
+        });
+
+      this.pullCart();
   }
 
 }
