8b8fa261731ca6a94be85a2cc17cb27a55ba9f3f
santoscyrilray <78798118+santoscyrilray@users.noreply.github.com>
2021-11-16 22:21:46 +0800

    snack/drink page -- landing page/landing nav

--- TeaBob-Responsive/src/app/pages/snacks/snacks.component.ts
+++ TeaBob-Responsive/src/app/pages/snacks/snacks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-snacks',
   templateUrl: './snacks.component.html',
@@ -7,9 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SnacksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
+    this.pullFood();
+    this.pullCart();
+  }
+  
+  food: any;
+
+  pullFood(){
+    this.ds.sendApiRequest("food", null).subscribe((data: { payload: any; }) => {
+    this.food = data.payload;
+    })
+  }
+  prodinfo: any = {};
+
+  async delProd(e: any) {
+    this.prodinfo.prod_id = e;
+
+        this.ds.sendApiRequest("delProd", JSON.parse(JSON.stringify(this.prodinfo))).subscribe((data: any) => {
+        });
+
+      this.pullFood();
+  }
+
+  prodInfo: any = {};
+  title: any;
+  info: any;
+
+
+  addToCart(food:any) {
+
+
+    this.prodInfo.user_id = localStorage.getItem("id");
+    this.prodInfo.title = food.title;
+    this.prodInfo.description = food.description;
+    this.prodInfo.price = food.price;
+    
+
+    this.ds.sendApiRequest("addCart", JSON.parse(JSON.stringify(this.prodInfo))).subscribe((data: any) => {
+    });
+
+
+    console.log(this.prodInfo);
+
+
+  }
+
+  cartinfo: any={};
+  cart:any;
+  cartCounter: any;
+  
+
+  pullCart() {
+    this.cartinfo.user_id = localStorage.getItem("id");
+    this.ds.sendApiRequest("cart",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
+    this.cart = data.payload;
+
+    // this.getTotal();
+
+    // if(this.cart != null){
+
+    for (let i = 0; i <= this.cart.length; i++) {
+      this.cartCounter = i;
+      console.log(this.cartCounter);
+    }
+    }
+    )
   }
 
 }
