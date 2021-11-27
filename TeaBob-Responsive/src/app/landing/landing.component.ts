102353d5ca33968ed8b8c13b47bc7944e8949a1a
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-11-28 02:49:50 +0800

    featured2

--- TeaBob-Responsive/src/app/landing/landing.component.ts
+++ TeaBob-Responsive/src/app/landing/landing.component.ts
@@ -12,71 +12,73 @@ export class LandingComponent implements OnInit {
   constructor(private ds: DataService) { }
 
   ngOnInit(): void {
-    this.pullFood();
-    this.pullCart();
+    this.pullFoodFeatured();
+    // this.pullCart();
   }
   
   foods: any[]=[];
 
-  pullFood(){
-    this.ds.sendApiRequest("food", null).subscribe((data: { payload: any; }) => {
+  pullFoodFeatured(){
+    this.ds.sendApiRequest("foodfeatured", null).subscribe((data: { payload: any; }) => {
     this.foods = data.payload;
     })
   }
-  prodinfo: any = {};
-
-  async delProd(e: any) {
-    this.prodinfo.prod_id = e;
-
-        this.ds.sendApiRequest("delProd", JSON.parse(JSON.stringify(this.prodinfo))).subscribe((data: any) => {
-        });
-
-      this.pullFood();
-  }
-
-  prodInfo: any = {};
-  title: any;
-  info: any;
 
 
-  addToCart(food:any) {
+  // prodinfo: any = {};
+
+  // async delProd(e: any) {
+  //   this.prodinfo.prod_id = e;
+
+  //       this.ds.sendApiRequest("delProd", JSON.parse(JSON.stringify(this.prodinfo))).subscribe((data: any) => {
+  //       });
+
+  //     this.pullFood();
+  // }
+
+  // prodInfo: any = {};
+  // title: any;
+  // info: any;
 
 
-    this.prodInfo.user_id = localStorage.getItem("id");
-    this.prodInfo.title = food.title;
-    this.prodInfo.description = food.description;
-    this.prodInfo.price = food.price;
+  // addToCart(food:any) {
+
+
+  //   this.prodInfo.user_id = localStorage.getItem("id");
+  //   this.prodInfo.title = food.title;
+  //   this.prodInfo.description = food.description;
+  //   this.prodInfo.price = food.price;
     
 
-    this.ds.sendApiRequest("addCart", JSON.parse(JSON.stringify(this.prodInfo))).subscribe((data: any) => {
-    });
+  //   this.ds.sendApiRequest("addCart", JSON.parse(JSON.stringify(this.prodInfo))).subscribe((data: any) => {
+  //   });
 
 
-    console.log(this.prodInfo);
+  //   console.log(this.prodInfo);
 
 
-  }
+  // }
 
-  cartinfo: any={};
-  cart:any;
-  cartCounter: any;
+  // cartinfo: any={};
+  // cart:any;
+  // cartCounter: any;
   
 
-  pullCart() {
-    this.cartinfo.user_id = localStorage.getItem("id");
-    this.ds.sendApiRequest("cart",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
-    this.cart = data.payload;
+  // pullCart() {
+  //   this.cartinfo.user_id = localStorage.getItem("id");
+  //   this.ds.sendApiRequest("cart",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
+  //   this.cart = data.payload;
 
-    // this.getTotal();
+  //   // this.getTotal();
 
-    // if(this.cart != null){
+  //   // if(this.cart != null){
 
-    for (let i = 0; i <= this.cart.length; i++) {
-      this.cartCounter = i;
-      console.log(this.cartCounter);
-    }
-    }
-    )
-  }
+  //   for (let i = 0; i <= this.cart.length; i++) {
+  //     this.cartCounter = i;
+  //     console.log(this.cartCounter);
+  //   }
+  //   }
+  //   )
+  // }
 
 }
