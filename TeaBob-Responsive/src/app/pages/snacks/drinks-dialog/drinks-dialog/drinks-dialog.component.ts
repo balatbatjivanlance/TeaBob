61ae0c7f941767d7496d460e47a506b9de320360
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2021-11-29 17:21:27 +0800

    dagdag sa dialog

--- TeaBob-Responsive/src/app/pages/snacks/drinks-dialog/drinks-dialog/drinks-dialog.component.ts
+++ TeaBob-Responsive/src/app/pages/snacks/drinks-dialog/drinks-dialog/drinks-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-drinks-dialog',
@@ -6,10 +9,144 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drinks-dialog.component.css']
 })
 export class DrinksDialogComponent implements OnInit {
+  user_id = localStorage.getItem("UID");
 
-  constructor() { }
+  constructor(private ds: DataService, @Inject(MAT_DIALOG_DATA)public data: any) { }
 
   ngOnInit(): void {
+    this.pullUsers();
+    this.pullFood_perItem();
+  }
+
+  userinfo: any = {};
+  user: any;
+  pullUsers() {
+    this.userinfo.user_id = localStorage.getItem("id");
+    this.ds.sendApiRequest("users",localStorage.getItem("id")).subscribe((data: { payload: any; }) => {
+    this.user = data.payload;
+
+    }
+    )
+  }
+
+
+  foodinfo: any = {};
+  foods: any []=[];
+
+  food_name: any;
+  food_price: number = 0;
+  food_qty: number = 1;
+  food_total: number  = 0 ;
+
+
+  pullFood_perItem() {
+    this.userinfo.user_id = localStorage.getItem("id");
+    this.foodinfo.food_id = this.data.food_id
+    this.ds.sendApiRequest("food_item/", this.foodinfo.food_id).subscribe((data: { payload: any; }) => {
+    this.foods = data.payload;
+
+    this.food_name = this.foods[0].food_name;
+    this.food_price = this.foods[0].food_price;
+    this.food_total = this.food_price;
+
+    });
+
+  }
+
+  plusQty = () =>{
+    this.food_qty += 1;
+    if (this.food_qty > 10){
+      alert('You have reeached the maximum order');
+      this.food_qty -= 1;
+    }else{
+      this.food_total =  this.food_qty * this.food_price;
+    }
+    this.sendMessage();
+  }
+
+
+  
+  minusQty = () =>{
+    if (this.food_qty > 1){
+      this.food_qty -= 1;
+      this.food_total =  this.food_qty * this.food_price;
+      this.sendMessage();
+    }
+  }
+
+  addPearl = () =>{
+    var element = <HTMLInputElement> document.getElementById("extraPearl");
+    var isChecked = element.checked;
+  
+    if (isChecked == true){
+      let addSauce = 10;
+      this.food_total = this.food_total + addSauce;
+      console.log(this.food_total);
+    }else if (isChecked == false){
+        let addSauce =  10;
+        this.food_total =  this.food_total - addSauce;
+    }
+    this.sendMessage();
+  }
+  addCPuff = () =>{
+    var element = <HTMLInputElement> document.getElementById("extraCPuff");
+    var isChecked = element.checked;
+  
+    if (isChecked == true){
+      let addSauce = 10;
+      this.food_total = this.food_total + addSauce;
+      console.log(this.food_total);
+    }else if (isChecked == false){
+        let addSauce =  10;
+        this.food_total =  this.food_total - addSauce;
+    }
+    this.sendMessage();
+  }
+  addCcheese = () =>{
+    var element = <HTMLInputElement> document.getElementById("extraCcheese");
+    var isChecked = element.checked;
+  
+    if (isChecked == true){
+      let addSauce = 10;
+      this.food_total = this.food_total + addSauce;
+      console.log(this.food_total);
+    }else if (isChecked == false){
+        let addSauce =  10;
+        this.food_total =  this.food_total - addSauce;
+    }
+    this.sendMessage();
+  }
+  addCookies = () =>{
+    var element = <HTMLInputElement> document.getElementById("extraCookies");
+    var isChecked = element.checked;
+  
+    if (isChecked == true){
+      let addSauce = 10;
+      this.food_total = this.food_total + addSauce;
+      console.log(this.food_total);
+    }else if (isChecked == false){
+        let addSauce =  10;
+        this.food_total =  this.food_total - addSauce;
+    }
+    this.sendMessage();
+  }
+  
+  prodInfo: any = {};
+  title: any;
+  info: any;
+
+
+  addToCart() {
+    this.prodInfo.user_id = localStorage.getItem("id");
+    this.prodInfo.title = this.food_name;
+    // this.prodInfo.description = fooddescription;
+    this.prodInfo.price = this.food_price;
+    this.prodInfo.cart_total = this.food_total;
+    console.log(this.prodInfo);
+  }
+
+  sendMessage(): void {
+    this.ds.sendUpdate('Message from Sender Component to Receiver Component!')
   }
 
 }
