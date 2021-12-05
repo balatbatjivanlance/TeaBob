e1dfb280592bffe9491414f788bf1178a71a0208
santoscyrilray <78798118+santoscyrilray@users.noreply.github.com>
2021-12-05 20:10:06 +0800

    reg

--- TeaBob-Responsive/src/app/register/register.component.ts
+++ TeaBob-Responsive/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from "src/app/services/data.service";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -14,21 +15,34 @@ export class RegisterComponent implements OnInit {
   }
   userInfo: any = {};
   user_name: any;
+  user_lname: any;
+
   user_uname: any;
   user_contact: any;
   user_address: any;
   user_pword: any;
+  user_Confpword: any;
   regUser(){
-    this.userInfo.user_name = this.user_name;
+    if (this.user_pword == this.user_Confpword) {
+      this.userInfo.user_name = this.user_name;
+    this.userInfo.user_lname = this.user_lname;
     this.userInfo.user_uname = this.user_uname;
     this.userInfo.user_contact = this.user_contact;
     this.userInfo.user_address = this.user_address;
     this.userInfo.user_pword = this.user_pword;
+    this.userInfo.user_Confpword = this.user_Confpword;
+
 
     this.ds.sendApiRequest("regUser", JSON.parse(JSON.stringify(this.userInfo))).subscribe((data: any) => {
     });
 
-    console.log(this.userInfo);
+    Swal.fire('Register Successfully')
+      
+    }
+    
+    else
+    Swal.fire('Password did not match!')
+
   }
 
 }
