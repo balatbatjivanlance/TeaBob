acdba114096878ce4189d08ee5350468ea40ee4b
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2022-01-26 17:27:39 +0800

    new

---
+++ TeaBob-Mobile/src- orig/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'snack',
+    loadChildren: () => import('./pages/snack/snack.module').then( m => m.SnackPageModule)
+  },
+  {
+    path: 'drinks',
+    loadChildren: () => import('./pages/drinks/drinks.module').then( m => m.DrinksPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./pages/cart/cart.module').then( m => m.CartPageModule)
+  },
+  {
+    path: 'status',
+    loadChildren: () => import('./pages/status/status.module').then( m => m.StatusPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
