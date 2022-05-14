7e65c4f579a68e582ad193dfe69b7d7824ff03a0
balatbatjivanlance <78532392+balatbatjivanlance@users.noreply.github.com>
2022-05-14 22:05:42 +0800

    update email

--- TeaBob-Responsive/api/models/Mail.php
+++ TeaBob-Responsive/api/models/Mail.php
@@ -30,15 +30,15 @@ use PHPMailer\PHPMailer\Exception;
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = '201811140@gordoncollege.edu.ph';                  //SMTP username
-            $mail->Password   = 'balatbatGC2020';                               //SMTP password
+            $mail->Username   = 'darklightzzz40@gmail.com';                  //SMTP username
+            $mail->Password   = 'pass555word';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->setFrom('201811140@gordoncollege.edu.ph', 'TeaBob Email');
+            $mail->setFrom('darklightzzz40@gmail.com', 'TeaBob Email');
             $mail->addAddress($receiver);     //Add a recipient
-            $mail->addReplyTo('201811140@gordoncollege.edu.ph', 'TeaBob Email');
+            $mail->addReplyTo('darklightzzz40@gmail.com', 'TeaBob Email');
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
